458af1143756fa05a3540a92a2462b937601ca19
egkozlov <e.kozlov@softteco.com>
2015-05-22 11:23:55 +0300

    fixed SAM-34 Delivery Confirmation email update

--- business-logic/shipment/onPostSave.js
+++ business-logic/shipment/onPostSave.js
@@ -1,5 +1,7 @@
 function onPostSave(request, response, modules) {
     var logger = modules.logger, email = modules.email, body = request.body, collectionAccess = modules.collectionAccess, push = modules.push;
+    var context = modules.backendContext;
+    var http = modules.request;
     var userCollection = collectionAccess.collection("user");
     logger.info("query body " + JSON.stringify(body));
 
@@ -9,7 +11,7 @@ function onPostSave(request, response, modules) {
             function (err, route) {
                 if (err) {
                     logger("Query failed: " + err);
-                    response.complete(200);
+                    return response.complete(200);
                 }
                 logger.info("route info " + JSON.stringify(route));
                 userCollection.findOne({"_id": collectionAccess.objectID(body.driver._id)}, {},
@@ -20,20 +22,39 @@ function onPostSave(request, response, modules) {
                         if (driverEmail) {
                             logger.info("request body " + JSON.stringify(body));
                             logger.info("send email to " + driverEmail);
-                            //collectionAccess.collection('clients').findOne({"_id": collectionAccess.objectID(route.client._id)}, {},
-                            //    function (err, client) {
-                            //        if(err){
-                            //            logger.error("fetch client error " + err);
-                            //        }else{
-                            //            route.client = client.first_name + " " + client.last_name;
-                            //        }
-                                    email.send('do-not-reply@kinvey.com',
-                                        driverEmail,
-                                        'Delivery status',
-                                        'Your delivery from ' + route.start + " to " + route.finish + ' has been completed.');
-                                //});
+                            collectionAccess.collection('clients').findOne({"_id": collectionAccess.objectID(body.client._id)}, {},
+                                function (err, client) {
+                                    logger.info("Client " + JSON.stringify(client));
+                                    if(err){
+                                        logger.error("fetch client error " + err);
+                                    }else{
+                                        route.client = client.first_name + " " + client.last_name;
+                                        if(body.signature && body.signature._id){
+                                            getSignatureDownloadURL(body.signature._id,function(signature){
+                                                var htlmText = getHtmlText(route,signature)
+                                                email.send('do-not-reply@kinvey.com',
+                                                    driverEmail,
+                                                    'Delivery status',
+                                                    "Delivery status",
+                                                    null,
+                                                    htlmText);
+                                                return response.complete(200);
+                                            });
+                                        }else{
+                                            var htlmText = getHtmlText(route,body.signature);
+                                            email.send('do-not-reply@kinvey.com',
+                                                driverEmail,
+                                                'Delivery status',
+                                                "Delivery status",
+                                                null,
+                                                htlmText);
+                                            return response.complete(200);
+                                        }
+                                    }
+                                });
+                        }else{
+                            return response.complete(200);
                         }
-                        response.complete(200);
                     }
                 );
             });
@@ -45,10 +66,40 @@ function onPostSave(request, response, modules) {
                 if(request.method == "POST" && request.entityId === undefined){
                     push.sendMessage(driver, "You have a new pickup. Please go to your dispatch list to accept.");
                 }
-                response.complete(200);
+                return response.complete(200);
             }
         );
     } else {
-        response.complete(200);
+        return response.complete(200);
+    }
+
+    var getSignatureDownloadURL = function (signature_id, callback) {
+        var uri = 'https://' + context.getAppKey() + ':' + context.getMasterSecret() + '@' + request.headers.host
+            + '/blob/' + context.getAppKey() + '/?query={"_id":"' + signature_id + '"}&ttl_in_seconds=86400';
+
+        var req = http.get({url: uri},
+            function (error, responseBody, body) {
+                if (error) {
+                    logger.error("Query failed " + error);
+                    var result = {"error": error};
+                    return callback();
+                }
+
+                var results = JSON.parse(responseBody.body);
+                return callback(results[0]);
+            });
+    };
+
+    var getHtmlText = function(route, signature){
+        var htlmText = '<html>Your delivery has been completed.' +
+            '<ul><li>Delivery Date:' + route._kmd.lmt +
+            '</li><li>Customer Name:' + route.client +' </li>' +
+            '<li>Pickup Address:' + route.start +'</li>' +
+            '<li>Delivery Address:' + route.finish + '</li>';
+        if(signature){
+            htlmText+='<li>Signature:</br> <img src="' + signature._downloadURL + '"' + '/></li>';
+        }
+        htlmText+='</ul>'+ '</html>';
+        return htlmText;
     }
 }
